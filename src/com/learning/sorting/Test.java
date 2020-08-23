251474e7a7d39ffb0e83089dcd6047df3402d375
d.bystrican <d.bystrican@gmail.com>
2020-08-23 14:20:20 +0200

    TYPOS in print statements fixed

--- src/com/learning/sorting/Test.java
+++ src/com/learning/sorting/Test.java
@@ -6,11 +6,11 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args) {
         var arrayUnsorted = ArrayGenerator.generateIntArray(10, 0, 8);
-        System.out.println("Unsorted ramdomly generated array is "
+        System.out.println("Unsorted randomly generated array is "
                 + Arrays.toString(arrayUnsorted));
-        System.out.println("Sorted ramdomly generated array with bubblesort is "
+        System.out.println("Sorted randomly generated array with bubble sort is "
                 + Arrays.toString(IntegerSorter.bubbleSort(arrayUnsorted, 0, arrayUnsorted.length)));
-        System.out.println("Sorted ramdomly generated array with insertion sort is "
+        System.out.println("Sorted randomly generated array with insertion sort is "
                 + Arrays.toString(IntegerSorter.insertionSort(arrayUnsorted, 0, arrayUnsorted.length)));
     }
 }
